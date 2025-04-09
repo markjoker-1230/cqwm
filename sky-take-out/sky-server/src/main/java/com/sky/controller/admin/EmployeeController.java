ad32e32085fc97643958abd5707422295c7f16a2
LYT <598447493@qq.com>
2025-04-09 18:11:56 +0800

    启用禁用员工账号业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -101,4 +101,17 @@ public class EmployeeController {
         return Result.success(pageResult);
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号，状态为：{}，员工id为：{}",status,id);
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
 }
